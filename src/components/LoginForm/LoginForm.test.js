ffd87531d0a3010fcb7981f3e4851731bb6afde1
Kinshuk Goel <kinshukgoel4@gmail.com>
2022-12-20 00:25:37 +0530

    Added Email,Password Validation☑ along with Unit testing🧪  using Jest

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import LoginForm from '.';
+import EmailValidator from 'email-validator';
+
 
 test('renders sign in page', () => {
   render(<LoginForm />);
@@ -8,3 +10,34 @@ test('renders sign in page', () => {
 });
 
 // Add more unit test here
+
+
+test('validateForm', () => {
+  const data = {
+    email: 'test@example.com',
+    password: 'Test123!'
+  };
+  const event = {
+    preventDefault: jest.fn(),
+    currentTarget: data
+  };
+
+  validateForm(event);
+  expect(event.preventDefault).toHaveBeenCalled();
+  expect(showAlert).toBe(false);
+
+  data.email = 'invalid';
+  validateForm(event);
+  expect(showAlert).toBe(true);
+
+  data.email = 'test@example.com';
+  data.password = 'invalid';
+  validateForm(event);
+  expect(showAlert).toBe(true);
+});
+
+
+
+
+
+
