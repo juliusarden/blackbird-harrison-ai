ae673e445a932c53b70840eae8f6f9b9770ffb5d
Dikshant1408 <91022943+Dikshant1408@users.noreply.github.com>
2023-06-08 14:50:39 +0530

    new

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,10 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import LoginForm from '.';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import LoginForm from './LoginForm';
 
-test('renders sign in page', () => {
-  render(<LoginForm />);
-  const signInText = screen.getByText("Sign in");
-  expect(signInText).toBeInTheDocument();
+describe('LoginForm', () => {
+  it('displays error message for invalid email', () => {
+    const { getByLabelText, getByText } = render(<LoginForm />);
+    const emailInput = getByLabelText('Email');
+    const submitButton = getByText('Login');
+
+    fireEvent.change(emailInput, { target: { value: 'invalidemail' } });
+    fireEvent.click(submitButton);
+
+    expect(getByText('Invalid email address')).toBeInTheDocument();
+  });
+
+  it('displays error message for invalid password', () => {
+    const { getByLabelText, getByText } = render(<LoginForm />);
+    const passwordInput = getByLabelText('Password');
+    const submitButton = getByText('Login');
+
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.click(submitButton);
+
+    expect(getByText('Invalid password')).toBeInTheDocument();
+  });
+
+  it('displays success message for valid email and password', () => {
+    const { getByLabelText, getByText } = render(<LoginForm />);
+    const emailInput = getByLabelText('Email');
+    const passwordInput = getByLabelText('Password');
+    const submitButton = getByText('Login');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'Password1!' } });
+    fireEvent.click(submitButton);
+
+    expect(getByText('Login successful')).toBeInTheDocument();
+  });
 });
-
-// Add more unit test here
