152d875a50137bee7f02606b879ce69fd3f6e939
Mohammad Ali <63194288+Mohammad74Ali@users.noreply.github.com>
2023-09-04 07:31:37 +0500

    Update App.js
    This code provides a basic structure for email and password validation in a React component using Material UI components and the email-validator library. You will need to integrate this code into your project, add any missing dependencies, and expand upon it as needed for your specific requirements.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,99 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import LoginForm from './components/LoginForm';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import emailValidator from "email-validator";
 
-const theme = createTheme();
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [error, setError] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Validate email
+    const isEmailValid = emailValidator.validate(email);
+
+    // Validate password based on requirements
+    const isPasswordValid =
+      password.length >= 8 &&
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /\d/.test(password) &&
+      /[!@#$%^&*]/.test(password);
+
+    if (isEmailValid && isPasswordValid) {
+      setOpenSnackbar(true);
+      setSnackbarMessage("Validation passed!");
+    } else {
+      setError({
+        email: !isEmailValid,
+        password: !isPasswordValid,
+      });
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <LoginForm />
-      </Grid>
-    </ThemeProvider>
+    <div>
+      <TextField
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={handleEmailChange}
+        error={error.email}
+        helperText={error.email ? "Invalid email address" : ""}
+      />
+      <br />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+        error={error.password}
+        helperText={
+          error.password
+            ? "Invalid password (min 8 characters, uppercase, lowercase, digit, special character)"
+            : ""
+        }
+      />
+      <br />
+      <Button variant="contained" color="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert onClose={handleCloseSnackbar} severity="success">
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 }
 
 export default App;
+    
